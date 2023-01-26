d7e1ca8a0de1f4c1ab48951addbdb966c1b7b7e7
Bartłomiej Stępień <masafion@gmail.com>
2023-01-26 15:49:31 +0100

    first commit

---
+++ src/main/java/com/szczurek/rats/RatsApplication.java
@@ -0,0 +1,13 @@
+package com.szczurek.rats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RatsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/szczurek/rats/RatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szczurek.rats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
